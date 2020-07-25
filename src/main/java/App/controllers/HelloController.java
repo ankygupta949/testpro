9b2f2d82541cd93efcd82e10a97e9a51ea5663d3
ankitg <gupta.ankit949@gmail.com>
2020-07-25 15:42:14 +0530

    third commit

--- src/main/java/App/controllers/HelloController.java
+++ src/main/java/App/controllers/HelloController.java
@@ -60,7 +60,7 @@ public class HelloController {
     @RequestMapping("/book")
     public BookDTO getBook() {
         BookDTO bookDTO = new BookDTO();
-        bookDTO.setBookName("Harry Porter First Book");
+        bookDTO.setBookName("Harry Porter The Soccer stone");
         bookDTO.setBookPrice(new BigDecimal("120.00"));
         return bookDTO;
     }
